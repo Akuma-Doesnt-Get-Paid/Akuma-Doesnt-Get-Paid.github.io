5243ee7fc96dc959d80e18f8b4b8f435f7f47926
Akuma-Doesnt-Get-Paid <maxisgood10013120096969@gmail.com>
2025-02-10 21:52:07 -0500

    Update script.js

--- v2/assets/script.js
+++ v2/assets/script.js
@@ -2,7 +2,7 @@ const bt = document.getElementById('battery');  
 function updateBatteryStatus(battery) {  
   bt.innerHTML = `${Math.floor(battery.level * 100)}%`; 
   if (battery.charging) {    
-    bt.innerHTML += ' (It's in 👀)';  
+    bt.innerHTML += ' (Its in 👀)';  
   }    }  
 function batteryUpdate() {   
   navigator.getBattery().then(battery => {     
